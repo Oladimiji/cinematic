f9910bd91721ad7c4e3d63285eeb65ee1e5fdf50
Olorunto Noah Oladimeji <108664870+Oladimiji@users.noreply.github.com>
2025-01-13 05:21:05 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    const itemNumber = movieLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
+    arrow.addEventListener("click", () => {
+        clickCounter++;
+        if(itemNumber - (4 + clickCounter) +(4 - ratio) >= 0) {
+        movieLists[i].style.transform = `translateX(${
+          movieLists[i].computedStyleMap().get("transform")[0].x.value
+        -300}px)`;
+    } else{
+        movieLists[i].style.transform = "translateX(0)";
+        clickCounter = 0;
+    }
+
+    });
+});
+
+
+//toggle
+
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+    ".ready,.arr,.eng,.freq,.container,.menu-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle,.toggle"
+);
+
+ball.addEventListener("click",()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
+    })
+    ball.classList.toggle("active")
+})
